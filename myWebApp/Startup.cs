b2350421f520af3254db96b06e71e7d3bdce53d8
Roberto Fiocchi <r.fiocchi@mdsnet.eu>
2016-02-04 15:31:45 +0100

    Init Solution

---
+++ myWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(myWebApp.Startup))]
+namespace myWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
